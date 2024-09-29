4c695e432701058c45807184e93291bb3d9b7d03
DANIEL KELLY <G00421651@atu.ie>
2024-09-29 20:40:26 +0100

    Q2c

--- Lab 1/ArrowFunction.js
+++ Lab 1/ArrowFunction.js
@@ -5,3 +5,8 @@ console.log(getName());
 //2b
 const returnArgument = (argument) => argument;
 console.log(returnArgument("Data Representation and Querying"));
+
+//2c Return sum of 2 numbers using arrow function
+const getSum = (num1, num2) => num1 * num2;
+console.log(getSum(5,5));//Sum should return 25
+
