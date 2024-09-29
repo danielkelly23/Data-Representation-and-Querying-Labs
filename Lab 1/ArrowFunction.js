8dddaa00636db83196757cb7b86304a875f27f9a
DANIEL KELLY <G00421651@atu.ie>
2024-09-29 20:30:51 +0100

    2b

--- Lab 1/ArrowFunction.js
+++ Lab 1/ArrowFunction.js
@@ -1,3 +1,7 @@
 //Q2a Arrow function returning String
 const getName = () => "Data Representation and Querying";
 console.log(getName());
+
+//2b
+const returnArgument = (argument) => argument;
+console.log(returnArgument("Data Representation and Querying"));
