740aea20947bbc6c36b375b401c75def96f528ef
DANIEL KELLY <G00421651@atu.ie>
2024-09-29 20:56:15 +0100

    2d .map

--- Lab 1/ArrowFunction.js
+++ Lab 1/ArrowFunction.js
@@ -10,3 +10,16 @@ console.log(returnArgument("Data Representation and Querying"));
 const getSum = (num1, num2) => num1 * num2;
 console.log(getSum(5,5));//Sum should return 25
 
+//2d .map function to multiply each number under 70 by 2
+const ages = [25, 31, 42, 77];
+const multiplyUnder70 = ages.map(age =>
+{   if(age < 70)
+    {
+        return age * 2;
+    }
+    else
+    {
+        return age;
+    }
+    });
+console.log(multiplyUnder70);
