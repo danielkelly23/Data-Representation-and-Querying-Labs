6c8c9456bc046caabc244935063d0216c213cf14
DANIEL KELLY <G00421651@atu.ie>
2024-10-06 20:34:40 +0100

    Componentization

--- Lab2/my-app/src/App.js
+++ Lab2/my-app/src/App.js
@@ -1,11 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import Header from './components/Header';
+import Content from './components/Content';
+import Footer from './components/Footer';
 
 function App() {
   return (
     <div>
-      <h1>Hello World</h1>
-      <h2>It is {new Date().toLocaleTimeString()}.</h2>
+      <Header />
+      <Content />
+      <Footer />
     </div>
   );
 }
